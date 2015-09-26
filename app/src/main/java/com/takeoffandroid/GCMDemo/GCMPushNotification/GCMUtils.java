a54924547a41a06fa55818c8a1a491d462667454
TakeoffAndroid <takeoffandroid@gmail.com>
2015-09-26 14:08:33 +0530

    Commit for blog

--- app/src/main/java/com/takeoffandroid/GCMDemo/GCMPushNotification/GCMUtils.java
+++ app/src/main/java/com/takeoffandroid/GCMDemo/GCMPushNotification/GCMUtils.java
@@ -5,14 +5,11 @@ public class GCMUtils {
  
     public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
     public static final String REGISTRATION_COMPLETE = "registrationComplete";
-    public static final String PROJECT_ID = "519458252716";
+    public static final String PROJECT_ID = "YOUR_PROJECT_ID"; //EXAMPLE: 123456789101
 
    // http://www.androidbegin.com/tutorial/gcm.html
 
-//    519458252716
 
-//    fToX1ynuH9M:APA91bGBzlxaF6Ia8VFfnN-RVPXDuqkoBdbh835pefmthNU70xGgxV2cs9ZpJJJ5Vr3FZzgwV85Tb7Gt4oFy12948kwOVuI4Zvrj7x1qtSfyjppPFRfqzoZew_jFds-YM27DIPz2cIUy
-//    AIzaSyCzeGLhVqzlO8Tum33QtiAjFh-L768j2xg
 
  
 } 
